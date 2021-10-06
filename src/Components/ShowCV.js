86f0d0a79a7fba90149dd2231de2b185707fb051
jahid-044 <jahidruetian044@gmail.com>
2021-10-06 18:51:44 +0600

    first day done

--- src/Components/ShowCV.js
+++ src/Components/ShowCV.js
@@ -3,8 +3,38 @@
 export default function ShowCV() {
     return (
         <>
-            <div className="flex flex-col">
-                <div>
+            <div className="flex text-white w-3/5 m-auto">
+                <div className=" bg-gray-700 flex flex-col justify-evenly h-screen p-8">
+                    <div className="">
+                        <h1>ABOUT ME</h1>
+                        <p>
+                            Date of birth: 11/10/1998 <br />
+                            Age: 23 years <br />
+                            Gender: Male <br />
+                            Birth place: Chuadanga,Khulna
+                        </p>
+                    </div>
+                    <div>
+                        <h1>CONTACT</h1>
+                        <p>
+                            Damurhuda, Chuadanga <br />
+                            0183791984 <br />
+                            jahidruetian044@gmail.com
+                        </p>
+                    </div>
+                    <div >
+                        <h1>INTERESTS</h1>
+                        <p>
+                            <ul className="list-none">
+                                <li>Problem solving</li>
+                                <li>Sleeping</li>
+                                <li>Chatting with friendsy</li>
+                            </ul>
+                        </p>
+                    </div>
+
+                </div>
+                <div className="text-black">
 
                 </div>
             </div>
