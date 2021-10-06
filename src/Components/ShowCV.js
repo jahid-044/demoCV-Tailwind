b5651fcd501cb8973cd269d944326ce1170e7b7f
jahid-044 <jahidruetian044@gmail.com>
2021-10-06 16:59:35 +0600

    deleting files done

--- src/Components/ShowCV.js
+++ src/Components/ShowCV.js
@@ -3,7 +3,11 @@
 export default function ShowCV() {
     return (
         <>
+            <div className="flex flex-col">
+                <div>
 
+                </div>
+            </div>
         </>
     )
 }
